9af48d328a6c9b95285215f9c76529997416d450
Nathan Dantas <than_dantas@hotmail.com>
2021-07-15 20:41:20 -0300

    adicionando programa de interface funcional

---
+++ Programas Simples/ApIJava/src/Aula2/FuncaoAltaOrdem.java
@@ -0,0 +1,22 @@
+package Aula2;
+
+public class FuncaoAltaOrdem {
+    public static void main(String[]args){
+        Calculo soma = (a,b) -> a+b;
+        Calculo subtrai = (a,b) -> a-b;
+        Calculo multiplica = (a,b) -> a*b;
+        Calculo divide = (a,b) -> a/b;
+        System.out.println(executarOperacao(soma, 1, 3));
+        System.out.println(executarOperacao(subtrai, 4, 3));
+        System.out.println(executarOperacao(divide, 4, 2));
+        System.out.println(executarOperacao(multiplica, 7, 3));
+
+    }
+
+    public static int executarOperacao(Calculo calculo, int a, int b){
+        return calculo.calcular(a,b);
+    }
+}
+interface Calculo {
+    public int calcular(int a, int b);
+}
