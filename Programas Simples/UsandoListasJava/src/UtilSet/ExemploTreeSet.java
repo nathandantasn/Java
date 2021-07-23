895dcc9c62bc91df32db8830d2fa527505d46d9c
Nathan Dantas <than_dantas@hotmail.com>
2021-07-22 23:13:58 -0300

    adicionando treeSet

--- Programas Simples/UsandoListasJava/src/UtilSet/ExemploTreeSet.java
+++ Programas Simples/UsandoListasJava/src/UtilSet/ExemploTreeSet.java
@@ -1,10 +1,41 @@
 package UtilSet;
 
 
+import java.util.TreeSet;
 
 public class ExemploTreeSet {
 
     public static void main (String[]args) {
+        TreeSet<String> treeCapitais = new TreeSet<>();
 
+        treeCapitais.add("Porto Alegre");
+        treeCapitais.add("Florianópolis");
+        treeCapitais.add("Curitiba");
+        treeCapitais.add("São Paulo");
+        treeCapitais.add("Rio de Janeiro");
+        treeCapitais.add("Belo Horizonte");
+
+        System.out.println(treeCapitais);
+
+        // Retorna a primeira capital no topo da árvore
+        System.out.println(treeCapitais.first());
+
+        // Retorna a ultima capital no final da árvore
+        System.out.println(treeCapitais.last());
+
+        // Retorna a primeira capital abaixo na árvore da capital parametizada
+        System.out.println(treeCapitais.lower("Florianópolis"));
+
+        // Retorna a primeira capital acima na árvore da capital parametizada
+        System.out.println(treeCapitais.higher("Florianópolis"));
+
+        // Retorna a primeira capital no topo da árvore, removendo do set
+        System.out.println(treeCapitais.pollFirst());
+
+        // Retorna a primeira capital no final da árvore, removendo do set
+        System.out.println(treeCapitais.pollLast());
+
+        // Exibe todas as capitais no console
+        System.out.println(treeCapitais);
     }
 }
